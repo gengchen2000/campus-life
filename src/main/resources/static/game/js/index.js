8ff639da91cbf546c39b7e690837be21d6b66efc
仰天长啸两三声 <DaBuNer@163.com>
2022-10-27 14:44:09 +0800

    进度条

--- src/main/resources/static/game/js/index.js
+++ src/main/resources/static/game/js/index.js
@@ -5,8 +5,16 @@ let topNumber = 0;
 
 let interval;
 
-const n = 8;
-
+const n = 1;
+let width = 0
+let p1 = document.getElementById('p1');
+p1.style.width = '0%'
+setInterval(function () {
+    p1.style.width = ++width + '%';
+    if (p1.style.width === '100%') {
+        width = 0
+    }
+}, 200)
 
 function changeGreen() {
     box.style.backgroundColor = '#449d44'
@@ -22,29 +30,28 @@ function run(type) {
             interval = setInterval(function () {
                 leftNumber -= n
                 box.style.left = leftNumber + 'px'
-            }, 10)
+            }, 0.1)
             break
         case 'R':
             interval = setInterval(function () {
                 leftNumber += n
                 box.style.left = leftNumber + 'px'
-            }, 10)
+            }, 0.1)
             break
         case 'B':
             interval = setInterval(function () {
                 topNumber += n
                 box.style.top = topNumber + 'px'
-            }, 10)
+            }, 0.1)
             break
         case 'T':
             interval = setInterval(function () {
                 topNumber -= n
                 box.style.top = topNumber + 'px'
-            }, 10)
+            }, 0.1)
             break
     }
 }
-
 
 function outInterval() {
     clearInterval(interval)
