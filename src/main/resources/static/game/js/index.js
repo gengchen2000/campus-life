b377304162190dc5637f0d9a756867122812a674
仰天长啸两三声 <DaBuNer@163.com>
2022-10-27 10:20:43 +0800

    按钮改变颜色

---
+++ src/main/resources/static/game/js/index.js
@@ -0,0 +1,9 @@
+var box = document.getElementById('box');
+
+function changeGreen() {
+    box.style.backgroundColor = '#449d44'
+}
+
+function changeYellowOrange() {
+    box.style.backgroundColor = '#ec971f'
+}
