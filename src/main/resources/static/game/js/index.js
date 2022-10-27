04261b093a91491a1054c0cdb612fbc813c7c272
仰天长啸两三声 <DaBuNer@163.com>
2022-10-27 10:57:04 +0800

    移动按钮

--- src/main/resources/static/game/js/index.js
+++ src/main/resources/static/game/js/index.js
@@ -1,9 +1,33 @@
 var box = document.getElementById('box');
 
+var leftNumber = 0
+var topNumber = 0
+
+
 function changeGreen() {
     box.style.backgroundColor = '#449d44'
 }
 
 function changeYellowOrange() {
     box.style.backgroundColor = '#ec971f'
+}
+
+function runTop() {
+    topNumber -= 4
+    box.style.top = topNumber + 'px'
+}
+
+function runBottom() {
+    topNumber += 4
+    box.style.top = topNumber + 'px'
+}
+
+function runRight() {
+    leftNumber += 4
+    box.style.left = leftNumber + 'px'
+}
+
+function runLeft() {
+    leftNumber -= 4
+    box.style.left = leftNumber + 'px'
 }
